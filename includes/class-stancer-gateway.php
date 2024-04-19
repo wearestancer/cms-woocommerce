07673d011a37c1309fccc1854025e26b74ae6725
Sebastien <sderen@stancer.com>
2024-04-19 10:24:08 +0200

    [fix] Redirect into the correct URL to fix payment not captured.

--- includes/class-stancer-gateway.php
+++ includes/class-stancer-gateway.php
@@ -122,7 +122,7 @@ class WC_Stancer_Gateway extends WC_Payment_Gateway {
 		}
 
 		return [
-			'receipt' => $order->get_checkout_order_received_url(),
+			'receipt' => $order->get_checkout_payment_url( true ),
 			'redirect' => $redirect,
 			'reload' => $reload,
 			'result' => $reload ? 'failed' : 'success',
