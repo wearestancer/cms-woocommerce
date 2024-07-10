82ea1a31e575ff2437a77bf5de90b24e185354c1
Sebastien <sderen@stancer.com>
2024-07-10 10:00:08 +0000

    FIX: restrict loading of admin, scope admin script.

--- src/ts/admin.ts
+++ src/ts/admin.ts
@@ -1,18 +1,16 @@
-type Keys = HTMLInputElement[];
+(($) => $(() => {
+  type Input = JQuery<HTMLInputElement>;
 
-const form = document.getElementById('mainform') as HTMLFormElement;
-const prefix = 'woocommerce_stancer';
-const testMode = document.getElementById(prefix + '_test_mode') as HTMLInputElement;
-const testKeys: Keys = [
-  document.getElementById(prefix + '_api_test_public_key') as HTMLInputElement,
-  document.getElementById(prefix + '_api_test_secret_key') as HTMLInputElement,
-];
-const liveKeys: Keys = [
-  document.getElementById(prefix + '_api_live_public_key') as HTMLInputElement,
-  document.getElementById(prefix + '_api_live_secret_key') as HTMLInputElement,
-];
+  const prefix = 'woocommerce_stancer';
+  const testMode: Input = $(`#${prefix}_test_mode`);
 
-const requireKey = (keys: Keys) => keys.map((key) => key.setAttribute('required','required'));
-const unRequireKey = (keys: Keys) => keys.map((key) => key.removeAttribute('required'));
+  const liveKeys: Input[] = [
+    $(`#${prefix}_api_live_public_key`),
+    $(`#${prefix}_api_live_secret_key`),
+  ];
 
-testMode?.addEventListener('input',() => testMode.checked ? unRequireKey(liveKeys) : requireKey(liveKeys));
+  const requireKey = (keys: Input[]) => keys.map((key) => key.attr('required', 'required'));
+  const unRequireKey = (keys: Input[]) => keys.map((key) => key.removeAttr('required'));
+
+  testMode?.on('input', () => testMode.is(':checked',) ? unRequireKey(liveKeys) : requireKey(liveKeys));
+}))(jQuery);
