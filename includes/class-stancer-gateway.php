7694c228a2bb736566743c5e5865b41a66304d24
Sebastien <sderen@stancer.com>
2024-07-24 13:18:34 +0000

    Add: use parent method to check if our gateway is available.

--- includes/class-stancer-gateway.php
+++ includes/class-stancer-gateway.php
@@ -693,7 +693,7 @@ class WC_Stancer_Gateway extends WC_Payment_Gateway {
 			return false;
 		}
 
-		return $this->api_config->is_configured();
+		return parent::is_available() && $this->api_config->is_configured();
 	}
 
 	/**
